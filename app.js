c7b3eaa483c587406d264dbc93d230523d8a4468
vpragya94 <83279911+vpragya94@users.noreply.github.com>
2021-11-15 20:06:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    $('#nav-search-select').change(function() {
+  
+      var selectedText = $(this).find('option:selected').text();
+  
+      $('#nav-search').find('.nav-search-label').html(selectedText);
+  
+    });
+  
+  });
